28f82e0112d59a62c707177974100de12587aa8f
chrissilence <christopherharison@gmail.com>
2026-01-14 02:44:37 -0500

    Nav updates

--- js/pages/header.js
+++ js/pages/header.js
@@ -2,8 +2,27 @@
     Portfolio.App.controller('HeaderController', function ($scope) {
         Portfolio.HeaderScope = $scope;
         Portfolio.HeaderScope.WebsiteName = "ChrisSilence";
+        Portfolio.HeaderScope.YouTubeLink = "https://www.youtube.com/chrissilence";
+        Portfolio.HeaderScope.TwitchLink = "https://www.twitch.tv/chrissilence";
+        Portfolio.HeaderScope.IsViewLevelEditorOff = false;
+        Portfolio.HeaderScope.IsViewDiscordBotOff = true;
 
         Portfolio.HeaderScope.init = function () {
+
+        };
+
+        Portfolio.HeaderScope.ShowViewLevelEditor = function () {
+           Portfolio.HeaderScope.IsViewLevelEditorOff = false;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+
+            $('#test').load('leveleditor.html', function() {
+                
+            });
+        };
+
+        Portfolio.HeaderScope.ShowViewDiscordBot = function () {
+           Portfolio.HeaderScope.IsViewLevelEditorOff = true;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = false;
 
         };
     });
