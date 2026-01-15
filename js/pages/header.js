db952be69c9d16406bf5f9cc943b8ba64404fbd4
chrissilence <christopherharison@gmail.com>
2026-01-14 21:41:17 -0500

    Various pages added.

--- js/pages/header.js
+++ js/pages/header.js
@@ -6,18 +6,41 @@
         Portfolio.HeaderScope.TwitchLink = "https://www.twitch.tv/chrissilence";
         Portfolio.HeaderScope.TwitterLink = "https://x.com/ChrisSilenceTV";
         Portfolio.HeaderScope.InstagramLink = "https://www.instagram.com/chrissilencetv";
-        Portfolio.HeaderScope.IsViewLevelEditorOff = false;
+
+        // VIEW VARIABLES
+
+        Portfolio.HeaderScope.IsViewAboutOff = false;
+        Portfolio.HeaderScope.IsViewLevelEditorOff = true;
         Portfolio.HeaderScope.IsViewDiscordBotOff = true;
         Portfolio.HeaderScope.IsViewAdobeOff = true;
+        Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+        Portfolio.HeaderScope.IsViewCagenixOff = true;
+        Portfolio.HeaderScope.IsViewGLSAOff = true;
 
         Portfolio.HeaderScope.init = function () {
+            Portfolio.HeaderScope.ShowViewAbout();
+        };
 
+        // VIEW FUNCTIONS
+
+        Portfolio.HeaderScope.ShowViewAbout = function () {
+            Portfolio.HeaderScope.IsViewAboutOff = false;
+           Portfolio.HeaderScope.IsViewLevelEditorOff = true;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+           Portfolio.HeaderScope.IsViewAdobeOff = true;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+           Portfolio.HeaderScope.IsViewCagenixOff = true;
+           Portfolio.HeaderScope.IsViewGLSAOff = true;
         };
 
         Portfolio.HeaderScope.ShowViewLevelEditor = function () {
+           Portfolio.HeaderScope.IsViewAboutOff = true;
            Portfolio.HeaderScope.IsViewLevelEditorOff = false;
            Portfolio.HeaderScope.IsViewDiscordBotOff = true;
            Portfolio.HeaderScope.IsViewAdobeOff = true;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+           Portfolio.HeaderScope.IsViewCagenixOff = true;
+           Portfolio.HeaderScope.IsViewGLSAOff = true;
 
           /* $('#ViewLevelEditor').load('leveleditor.html', function() {
                 
@@ -25,15 +48,53 @@
         };
 
         Portfolio.HeaderScope.ShowViewDiscordBot = function () {
+            Portfolio.HeaderScope.IsViewAboutOff = true;
            Portfolio.HeaderScope.IsViewLevelEditorOff = true;
            Portfolio.HeaderScope.IsViewDiscordBotOff = false;
            Portfolio.HeaderScope.IsViewAdobeOff = true;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+           Portfolio.HeaderScope.IsViewCagenixOff = true;
+           Portfolio.HeaderScope.IsViewGLSAOff = true;
         };
 
         Portfolio.HeaderScope.ShowViewAdobe = function() {
+            Portfolio.HeaderScope.IsViewAboutOff = true;
            Portfolio.HeaderScope.IsViewLevelEditorOff = true;
            Portfolio.HeaderScope.IsViewDiscordBotOff = true;
            Portfolio.HeaderScope.IsViewAdobeOff = false;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+           Portfolio.HeaderScope.IsViewCagenixOff = true;
+           Portfolio.HeaderScope.IsViewGLSAOff = true;
+        }
+
+        Portfolio.HeaderScope.ShowViewVideoEditing = function() {
+            Portfolio.HeaderScope.IsViewAboutOff = true;
+           Portfolio.HeaderScope.IsViewLevelEditorOff = true;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+           Portfolio.HeaderScope.IsViewAdobeOff = true;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = false;
+           Portfolio.HeaderScope.IsViewCagenixOff = true;
+           Portfolio.HeaderScope.IsViewGLSAOff = true;
+        }
+
+        Portfolio.HeaderScope.ShowViewCagenix = function() {
+            Portfolio.HeaderScope.IsViewAboutOff = true;
+           Portfolio.HeaderScope.IsViewLevelEditorOff = true;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+           Portfolio.HeaderScope.IsViewAdobeOff = true;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+           Portfolio.HeaderScope.IsViewCagenixOff = false;
+           Portfolio.HeaderScope.IsViewGLSAOff = true;
+        }
+
+        Portfolio.HeaderScope.ShowViewGLSA = function() {
+            Portfolio.HeaderScope.IsViewAboutOff = true;
+           Portfolio.HeaderScope.IsViewLevelEditorOff = true;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+           Portfolio.HeaderScope.IsViewAdobeOff = true;
+           Portfolio.HeaderScope.IsViewVideoEditingOff = true;
+           Portfolio.HeaderScope.IsViewCagenixOff = true;
+           Portfolio.HeaderScope.IsViewGLSAOff = false;
         }
     });
 })();
