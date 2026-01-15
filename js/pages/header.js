c652f446f2e9587d6ea0d65c5dbd9bc120d715c8
chrissilence <christopherharison@gmail.com>
2026-01-14 19:30:27 -0500

    Added Adobe View

--- js/pages/header.js
+++ js/pages/header.js
@@ -4,8 +4,11 @@
         Portfolio.HeaderScope.WebsiteName = "ChrisSilence";
         Portfolio.HeaderScope.YouTubeLink = "https://www.youtube.com/chrissilence";
         Portfolio.HeaderScope.TwitchLink = "https://www.twitch.tv/chrissilence";
+        Portfolio.HeaderScope.TwitterLink = "https://x.com/ChrisSilenceTV";
+        Portfolio.HeaderScope.InstagramLink = "https://www.instagram.com/chrissilencetv";
         Portfolio.HeaderScope.IsViewLevelEditorOff = false;
         Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+        Portfolio.HeaderScope.IsViewAdobeOff = true;
 
         Portfolio.HeaderScope.init = function () {
 
@@ -14,6 +17,7 @@
         Portfolio.HeaderScope.ShowViewLevelEditor = function () {
            Portfolio.HeaderScope.IsViewLevelEditorOff = false;
            Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+           Portfolio.HeaderScope.IsViewAdobeOff = true;
 
           /* $('#ViewLevelEditor').load('leveleditor.html', function() {
                 
@@ -23,6 +27,13 @@
         Portfolio.HeaderScope.ShowViewDiscordBot = function () {
            Portfolio.HeaderScope.IsViewLevelEditorOff = true;
            Portfolio.HeaderScope.IsViewDiscordBotOff = false;
+           Portfolio.HeaderScope.IsViewAdobeOff = true;
         };
+
+        Portfolio.HeaderScope.ShowViewAdobe = function() {
+           Portfolio.HeaderScope.IsViewLevelEditorOff = true;
+           Portfolio.HeaderScope.IsViewDiscordBotOff = true;
+           Portfolio.HeaderScope.IsViewAdobeOff = false;
+        }
     });
 })();
